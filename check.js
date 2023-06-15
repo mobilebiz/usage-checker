e0be197a536be3b942e83b8c09776a8acc433f2f
Katsumi Takahashi <katsumi@gij.com>
2023-06-15 14:56:33 +0900

    FirstCommit

---
+++ check.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { ACCOUNT_SID, AUTH_TOKEN } = process.env;
+
+console.log(`🐞 Started...`);
+
+const client = require('twilio')(ACCOUNT_SID, AUTH_TOKEN);
+
+let totalPrice = 0;
+client.usage.records.lastMonth.list().then(async (records) => {
+  records.filter((record) => record.count > 0).forEach((record) => {
+    console.log(`${record.category} count:${record.count} price:${record.price}`);
+    totalPrice += Math.ceil(record.price);
+  })
+})
+.finally(() => {
+  console.log(`🐞 Total price: ${totalPrice}`);
+});
+// console.log(`🐞 Total price: ${totalPrice}`);
